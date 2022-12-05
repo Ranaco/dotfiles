3cf39e0561033d2f1ede3074b314e8c9d5b742fc
Ranaco <ranasatyamraj@gmail.com>
2022-12-05 14:38:42 +0530

    Update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,15 @@
-require('craftzdog.base')
-require('craftzdog.highlights')
-require('craftzdog.maps')
-require('craftzdog.plugins')
+require('rana.base')
+require('rana.highlights')
+require('rana.maps')
+require('rana.plugins')
 
 local has = vim.fn.has
 local is_mac = has "macunix"
 local is_win = has "win32"
 
 if is_mac then
-  require('craftzdog.macos')
+  require('rana.macos')
 end
 if is_win then
-  require('craftzdog.windows')
+  require('rana.windows')
 end
